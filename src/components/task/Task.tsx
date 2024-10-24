0cb20ea6fe42196a392b0d9808e799ef6bf160ba
nischal <nischalkshaj5@gmail.com>
2024-10-25 04:31:19 +0530

    deploy:made change in the signup page

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -31,13 +31,11 @@ const Task = () => {
 
   const { get, post, put, del } = useApi();
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const parsedStartDate =
     date instanceof Date ? date : new Date(date || new Date());
   const [prevParsedStartDate, setPrevParsedStartDate] =
     useState(parsedStartDate);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const formatDateForAPI = (date: Date) => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
